3cf4ad5efa93106f1ff0ca764e8b37850c77db69
amiralam0 <sharpprogrammer2018@gmail.com>
2022-10-22 04:38:24 -0700

    users controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './modules/auth/user.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DatabaseModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
